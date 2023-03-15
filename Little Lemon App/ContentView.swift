2e198b7efb90425cf518ecd0bc718a5998e003a2
vsnaichuk <v.snaichuk@gmail.com>
2023-03-16 00:29:38 +0200

    Add image asset

--- Little Lemon App/ContentView.swift
+++ Little Lemon App/ContentView.swift
@@ -10,9 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("littleLemon")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
             Text("Hello, world!")
         }
         .padding()
