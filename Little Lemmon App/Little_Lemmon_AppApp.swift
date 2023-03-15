a3d815c389cd1c6173b6c09e80ea777a2c3dbe56
vsnaichuk <v.snaichuk@gmail.com>
2023-03-15 22:55:00 +0200

    Initial Commit

---
+++ Little Lemmon App/Little_Lemmon_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemmon_AppApp.swift
+//  Little Lemmon App
+//
+//  Created by vsn on 15.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemmon_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
